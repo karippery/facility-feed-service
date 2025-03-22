fb4ffc5fb6cafcd39ac79c63a3c5cc3e3d0e84a1
karippery <john.karippery@yahoo.com>
2025-03-22 21:24:00 +0100

    fix db config

--- facility_feed_service/settings.py
+++ facility_feed_service/settings.py
@@ -120,7 +120,7 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_DB', 'postgres'),
         'USER': os.environ.get('POSTGRES_USER', 'postgres'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
-        'HOST': 'localhost',
+        'HOST': os.environ.get('DB_HOST', 'db'),
         'PORT': '5432',
     }
 }
