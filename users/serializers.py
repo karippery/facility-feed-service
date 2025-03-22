c9468af7338cd68be8b763d2cc76a6f6091f7e12
karippery <john.karippery@yahoo.com>
2025-03-22 20:59:43 +0100

    fix flake8

--- users/serializers.py
+++ users/serializers.py
@@ -7,8 +7,8 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = [
-            "id", "email", "first_name", "last_name", "sex", "role", 
-            "is_verified", "is_staff", "is_superuser", "is_active", 
+            "id", "email", "first_name", "last_name", "sex", "role",
+            "is_verified", "is_staff", "is_superuser", "is_active",
             "created_date", "updated_date", "groups", "user_permissions"
         ]
         extra_kwargs = {
